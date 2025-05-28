9aedb3eca9a10ca2e0c8c9f3e586858bd1269d13
Jie0313 <lshijie203@gmail.com>
2025-05-29 00:45:24 +0800

    complete rpg function

--- static/js/rpg.js
+++ static/js/rpg.js
@@ -1,4 +1,4 @@
-let mapArray, ctx, currentImgMain, imgMain;
+let mapArray, ctx, currentImgMain, imgMain, currentMapIndex;
 const gridFullWidth = $("#canvas-rpg").width();
 const gridFullHeight = $("#canvas-rpg").height();
 const gridWidth = $("#canvas-rpg").width() / 10;
@@ -10,6 +10,10 @@ var sources = {
 };
 
 var images = {};
+var playerBlock = {
+    x:0,
+    y:0
+};
 
 function loadImages(sources, callback) {
     var loadedImages = 0;
@@ -33,41 +37,22 @@ function loadImages(sources, callback) {
     }
 }
 
-// Make sure jQuery is loaded before this script in your HTML file
-$(function(){
-    // 0 : available, 1 : start, 2 : final stop, 3: npc, 4: coin, 5: obstacle
-    mapArray = [
-        [0, 0, 1, 0, 0, 0, 0, 1, 0, 1],
-        [1, 0, 0, 0, 1, 0, 0, 0, 1, 1],
-        [1, 1, 0, 1, 1, 1, 1, 0, 0, 1],
-        [1, 0, 0, 1, 1, 1, 1, 1, 0, 1],
-        [1, 0, 1, 1, 1, 1, 1, 0, 0, 1],
-        [3, 0, 1, 1, 1, 1, 1, 0, 0, 1],
-        [1, 0, 0, 0, 0, 0, 0, 0, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 0, 0, 1],
-        [2, 0, 0, 0, 0, 4, 0, 0, 1, 1],
-        [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-    ];
-    ctx = $("#canvas-rpg")[0].getContext("2d");
-    ctx.canvas.width = $("#canvas-rpg").width();
-    ctx.canvas.height = $("#canvas-rpg").height();
+function findPlayerStart(map) {
+    for (let x in map) {
+        for (let y in map[x]) {
+            if (map[x][y] == 1) {
+                return { x: Number(x), y: Number(y) };
+            }
+        }
+    }
 
-    imgMain = new Image();
-    imgMain.src = "../static/img/spriteSheet.png";
-    currentImgMain = {
-        x: 0,
-        y: 0
-    };
+}
 
-    imgMain.onload = function(){
-        ctx.drawImage(imgMain, 0, 0, 80, 130, currentImgMain.x, currentImgMain.y, gridWidth, gridHeight);
-    };
-
-
-    // Load all images first, then draw the map and main character
+// Load all images first, then draw the map and main character
+function drawMapObjects(){
     for(let x in mapArray){
         for(let y in mapArray[x]){
-            if(mapArray[x][y] == 1){
+            if(mapArray[x][y] == 4){
                 loadImages(sources, function(images) {
                     ctx.drawImage(images.Mountain, 32, 65, 32, 32, y * gridWidth, x * gridHeight, gridWidth, gridHeight);
                 });
@@ -75,55 +60,104 @@ $(function(){
                 loadImages(sources, function(images) {
                     ctx.drawImage(images.Mountain, 32, 65, 32, 32, y * gridWidth, x * gridHeight, gridWidth, gridHeight);
                 });
-            }else if(mapArray[x][y] == 4){
+            }else if(mapArray[x][y] == 5){
                 loadImages(sources, function(images) {
                     ctx.drawImage(images.Mountain, 32, 65, 32, 32, y * gridWidth, x * gridHeight, gridWidth, gridHeight);
                 });
             }
         }
     }
+}
+
+function switchToNextMap(){
+    let nextIndex = 0;
+    do{
+        nextIndex = Math.floor(Math.random() * 5);
+    } while(nextIndex == currentMapIndex);
+    
+    currentMapIndex = nextIndex;
+    mapArray = mapList[currentMapIndex];
+
+    playerBlock = findPlayerStart(mapArray);
+    currentImgMain.x = playerBlock.y * gridWidth;
+    currentImgMain.y = playerBlock.x * gridHeight;
+
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+
+    drawMapObjects();
+
+    ctx.drawImage(imgMain, 0, 0, 80, 130, currentImgMain.x, currentImgMain.y, gridWidth, gridHeight);
+}
+
+// Make sure jQuery is loaded before this script in your HTML file
+$(function(){
+    ctx = $("#canvas-rpg")[0].getContext("2d");
+    ctx.canvas.width = $("#canvas-rpg").width();
+    ctx.canvas.height = $("#canvas-rpg").height();
+
+    currentMapIndex = Math.floor(Math.random()*5);
+    mapArray = mapList[currentMapIndex];
+
+    imgMain = new Image();
+    imgMain.src = "../static/img/spriteSheet.png";    //modify character
+    currentImgMain = {
+        x: 0,
+        y: 0
+    };
+
+    playerBlock = findPlayerStart(mapArray);
+    currentImgMain.x = playerBlock.y * gridWidth;
+    currentImgMain.y = playerBlock.x * gridHeight;
+
+    imgMain.onload = function(){
+        ctx.drawImage(imgMain, 0, 0, 80, 130, currentImgMain.x, currentImgMain.y, gridWidth, gridHeight);
+    };
+
+    drawMapObjects();
 });
 
 // Click Event
 $(document).on("keydown", function(event){
-    let targetImg, targetBlock, cutImagePositionX;
-    targetImg = {
-        x:-1,
-        y:-1
-    };
+    let targetBlock,cutImagePositionX,nextBlock;
     targetBlock = {
         x:-1,
         y:-1
     };
+    nextBlock = {
+        x : playerBlock.x,
+        y : playerBlock.y
+    };
+
     event.preventDefault();
+
     switch(event.code){
         case "ArrowLeft":
-            targetImg.x = currentImgMain.x - gridWidth;
-            targetImg.y = currentImgMain.y;
+            nextBlock.x = nextBlock.x;
+            nextBlock.y--;
             cutImagePositionX = 175;
             break;
         case "ArrowUp":
-            targetImg.x = currentImgMain.x;
-            targetImg.y = currentImgMain.y - gridHeight;
+            nextBlock.x--;
+            nextBlock.y = nextBlock.y;
             cutImagePositionX = 355;
             break;
         case "ArrowRight":
-            targetImg.x = currentImgMain.x + gridWidth;
-            targetImg.y = currentImgMain.y;
+            nextBlock.x = nextBlock.x;
+            nextBlock.y++;
             cutImagePositionX = 540;
             break;
         case "ArrowDown":
-            targetImg.x = currentImgMain.x;
-            targetImg.y = currentImgMain.y + gridHeight;
+            nextBlock.x++;
+            nextBlock.y = nextBlock.y;
             cutImagePositionX = 0;
             break;
         default:
             return;
     }
 
-    if(targetImg.x <= gridFullHeight && targetImg.x >=0 && targetImg.y <= gridFullWidth && targetImg.y >=0){
-        targetBlock.x = targetImg.y / gridHeight;
-        targetBlock.y = targetImg.x / gridWidth;
+    if(nextBlock.x < 10 && nextBlock.x >=0 && nextBlock.y < 10 && nextBlock.y >=0){
+        targetBlock.x = nextBlock.x;
+        targetBlock.y = nextBlock.y;
     }else{
         targetBlock.x = -1;
         targetBlock.y = -1;
@@ -133,21 +167,23 @@ $(document).on("keydown", function(event){
 
     if(targetBlock.x != -1 && targetBlock.y != -1){
         switch(mapArray[targetBlock.x][targetBlock.y]){
-            case 0:
-                currentImgMain.x = targetImg.x;
-                currentImgMain.y = targetImg.y;
-                break;
-            case 1:
+            case 0: // available
+            case 1: // start
+            case 4: // coin
+                currentImgMain.x = targetBlock.y * gridWidth;
+                currentImgMain.y = targetBlock.x * gridHeight;
+                playerBlock.x = targetBlock.x;
+                playerBlock.y = targetBlock.y;
                 break;
             case 2: // Final Stop
-                currentImgMain.x = targetImg.x;
-                currentImgMain.y = targetImg.y;
+                currentImgMain.x = targetBlock.y * gridWidth;
+                currentImgMain.y = targetBlock.x * gridHeight;
+                playerBlock.x = targetBlock.x;
+                playerBlock.y = targetBlock.y;
+                switchToNextMap();
                 break;
-            case 3: //Enemy
-                break;
-            case 4: // Tomato
-                currentImgMain.x = targetImg.x;
-                currentImgMain.y = targetImg.y;
+            case 3: // npc
+            case 5: // obstacle
                 break;
         }
     }else{
